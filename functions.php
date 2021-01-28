e6c3f343af60f64cc490c931374fa9280a8aeb21
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-01-28 15:37:33 +0100

    ajout commentaires fichier function

--- functions.php
+++ functions.php
@@ -5,15 +5,16 @@
 		<?php
 
 		/**
-		 * Functions hooked in to storefront_post_header_before action.
-		 *
-		 * @hooked storefront_post_meta - 10
+         * Fonction associée à l'action de storefront_post_header_before.
+         * Elle permet de ne pas afficher le titre sur la page d'accueil
+		 * Elle remplace la fonction storefront_post_header se trouvant dans la page storefront-template-functions.php
 		 */
 		do_action( 'storefront_post_header_before' );
-
+        //si c'est une simple page alors insertion de the_title
 		if ( is_single() ) {
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		} else {
+            //si ce n'est pas la page d'accueil, alors insertion des titres sur les autres pages
 			if (!is_front_page() ){
 				
 			the_title( sprintf( '<h2 class="alpha entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
@@ -24,5 +25,5 @@
 		?>
 		</header><!-- .entry-header -->
 		<?php
-	}
+	}   //ce add_action permet 
         add_action('storefront_loop_before','storefront_post_header');
